b6a2d698bc9d46ebbc87b531a345703b1da8af9f
DavieN99 <daviespym@gmail.com>
2023-02-21 17:51:09 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,19 @@
-#!/bin/bash
-#include <stdio.h>
-int main (void)
-{
-	puts("\"programming is like building a multilingual puzzle");
-	return(0);
-}
+/* 
+ *   * File: 4-puts.c 
+ *    * Auth: Davie Patrick  
+ *     */ 
+  
+ #include <stdio.h> 
+  
+ /** 
+  *   * main - Prints "Programming is like building a multilingual 
+  *    *                puzzle, followed by a new line. 
+  *     * 
+  *      * Return: Always 0. 
+  *       */ 
+ int main(void) 
+	 { 
+		          puts("\"Programming is like building a multilingual puzzle"); 
+		    
+		           return (0); 
+		    }
